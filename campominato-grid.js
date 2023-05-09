dbe62b0f3ca3e36bf142b67e5d29ac03025ab3ff
Luca Cassano <lucacassano96@gmail.com>
2023-05-09 22:30:47 +0200

    mancano i numeri

--- campominato-grid.js
+++ campominato-grid.js
@@ -9,32 +9,37 @@ function() {
 /* creo il contenitore della griglia */
 
 const main = document.querySelector("main");
-const gridContainer = `<div class="grid"></div>`;
+const gridContainer = `<div id="grid"></div>`;
  main.innerHTML += gridContainer;
 
- const container = document.querySelector(".grid");
+ const container = document.querySelector("#grid");
  console.log(container);
+
+ /* arrey numeri da 1 a 100 */
+
+ let arreyNumDaInserire =  listaNum("1", "100");
+ console.log(arreyNumDaInserire); 
+
 
 
  /* creo i quadrati che comporranno la griglia */
 
- for (let i = 1; i <= 100; i++) {
+ for (let i = 0; i < 100; i++) {
     
-    var newSquare = square("div", "square")
+    const newSquare = square("div", "square");
     
     newSquare.addEventListener("click",
     function() {
-    newSquare.classList.add("background")
-});
-
-    container.append(newSquare)
-
-  
-
- }
-
+    newSquare.classList.add("background");
 
     
+    }
+    );
+
+    container.append(newSquare);
+
+ }
+  
 
 })
 
@@ -46,12 +51,28 @@ const gridContainer = `<div class="grid"></div>`;
 
         /* creare la griglia */
 
-function square(square, classAdd) {
+function square(div, classAdd) {
 
 
-    const grid = document.createElement(square);
+    const grid = document.createElement(div);
     grid.classList.add(classAdd);
     return grid
 
     
 }
+
+        /* creare una lista di numeri ordinata  */
+function listaNum(min, max) {
+    
+    for (let i = min; i <= max; i++) {
+        
+        let element = [i]; 
+        console.log(element);
+        
+        return element;
+    }
+    
+}
+
+
+    
